acb88d30eeccade3a05fb239d27eaed28fe64ace
roaky404 <roaky404@gmail.com>
2025-12-23 21:14:51 +0300

    Create Node.svelte

---
+++ src/components/Node.svelte
@@ -0,0 +1,8 @@
+<script>
+  export let node;
+</script>
+
+<g transform="translate({node.x}, {node.y})">
+  <rect width="160" height="60" rx="10" ry="10" fill="#e0e0e0" />
+  <text x="10" y="35">{node.text}</text>
+</g>
