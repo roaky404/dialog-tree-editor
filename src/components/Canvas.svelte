48a48b80e6ca21e0471a50daf58c105ddac3a05d
roaky404 <roaky404@gmail.com>
2025-12-23 21:15:28 +0300

    Create Canvas.svelte

---
+++ src/components/Canvas.svelte
@@ -0,0 +1,22 @@
+<script>
+  import { dialogTree } from '../lib/store';
+  import Node from './Node.svelte';
+  import Link from './Link.svelte';
+
+  let tree;
+  dialogTree.subscribe(v => tree = v);
+
+  function getNode(id) {
+    return tree.nodes.find(n => n.id === id);
+  }
+</script>
+
+<svg width="100%" height="100%">
+  {#each tree.links as link}
+    <Link source={getNode(link.source)} target={getNode(link.target)} />
+  {/each}
+
+  {#each tree.nodes as node}
+    <Node node={node} />
+  {/each}
+</svg>
