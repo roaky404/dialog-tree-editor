dc90a7ad3dbf2c28d24cf524d2cdfcb6377ac266
roaky404 <roaky404@gmail.com>
2025-12-23 21:15:10 +0300

    Create Link.svelte

---
+++ src/components/Link.svelte
@@ -0,0 +1,12 @@
+<script>
+  export let source;
+  export let target;
+</script>
+
+<line
+  x1={source.x + 80}
+  y1={source.y + 30}
+  x2={target.x + 80}
+  y2={target.y + 30}
+  stroke="black"
+/>
