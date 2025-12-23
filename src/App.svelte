c18d2a5537671e6653c0e76d3b02f546ac741fe7
roaky404 <roaky404@gmail.com>
2025-12-23 21:13:36 +0300

    Create App.svelte

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+  import Canvas from './components/Canvas.svelte';
+</script>
+
+<main>
+  <Canvas />
+</main>
+
+<style>
+  main {
+    width: 100vw;
+    height: 100vh;
+    overflow: hidden;
+  }
+</style>
